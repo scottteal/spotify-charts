d896189500b39f31c63ac8e654d383bef828dbf5
Scott Teal <s.teal726@gmail.com>
2021-02-09 16:16:53 -0800

    Update spotify_sample_data.py

--- spotify_sample_data.py
+++ spotify_sample_data.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
@@ -11,60 +5,31 @@ import csv
 import pandas as pd
 import glob
 
-
-# In[2]:
-
-
+# Use a csv file included in this repo which includes all of the desired days of data, and their respective URL path to the associated CSV file on spotifycharts.com.
+# In the example file fot this repo, I am requesting the top 200 most streamed songs globally at daily granularity from 2020-01-01 to 2021-02-08.
 csv_file = 'spotify_urls.csv'
 
-
-# In[3]:
-
-
+# Turn the csv into a dataframe so that you will be able to loop through them.
 df = pd.read_csv(csv_file, names=["url", "date"])
 
-
-# In[4]:
-
-
-df.head()
-
-
-# In[5]:
-
-
+# Loop through the 'url' column of the dataframe to send requests to the url's in the column, downloading CSV files in the same directory
+# Then save the file using the 'date' as the filename with '.csv' as the file extension
 for index, row in df.iterrows():
     file = requests.get(row['url'])
     open(row['date'] + '.csv', 'wb').write(file.content)
 
-
-# In[9]:
-
-
+# In order to delete an unnecessary row in every CSV, and the header row in each CSV file, create a list of the CSV files you just downloaded by matching any file ending in .csv
 csv_files = glob.glob('*.csv')
 for file in csv_files:
     lines = open(file).readlines()
     open(file, 'w').writelines(lines[2:])
 
+# Combine all files in the csv_files glob into a single dataframe, while also appending the file name as a column so you know which date the data corresponds to
+combined_csv = []
+combined_csv = pd.concat([pd.read_csv(f).assign(charts_date=os.path.basename(f).split('.')[0]) for f in csv_files])
 
-# In[20]:
+# Check that the dataframe looks like the combined CSV you would expect
+combined_csv.head()
 
-
-csv_out = "spotify-sample-data.csv"
-csv_header = 'Position','Track Name','Artist','Streams','URL'
-with open(csv_out,"a") as f:
-    writer = csv.writer(f)
-    writer.writerow(csv_header)
-
-
-# In[27]:
-
-
-li = []
-
-for filename in csv_files:
-    df = pd.read_csv(filename, index_col=None)
-    li.append(df)
-
-frame = pd.concat(li, axis=0, ignore_index=True)
-
+# Export the dataframe that combined all of the CSV's into a single CSV called "combined_csv.csv"
+combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
